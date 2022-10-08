557f41642eedf64ffc81d89c37acf699ce7214f4
Monica Crowe <mcrowe9194@gmail.com>
2022-10-08 16:01:39 -0500

    First Commit

--- GitPractice/Program.cs
+++ GitPractice/Program.cs
@@ -6,7 +6,8 @@ namespace GitPractice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Initialization
+            //First Commit
         }
     }
 }
